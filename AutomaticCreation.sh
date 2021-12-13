4c27b921cad1d53cfab40327eedee4bf61d6e300
V <79286362+vincadian@users.noreply.github.com>
2021-12-14 00:36:48 +0100

    Update AutomaticCreation.sh

--- AutomaticCreation.sh
+++ AutomaticCreation.sh
@@ -4,5 +4,10 @@ sudo mv starport /usr/local/bin/
 wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 sudo export PATH=$PATH:/usr/local/go/bin
+sudo apt install nodejs
+
 starport version 
-go version   
+go version
+node -v
+
+node server.js
